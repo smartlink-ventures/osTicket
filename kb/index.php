096330cb112cee2ffb50af72cacd78c31a1344f9
Collier Devlin <cdevlin@smartlink.city>
2019-06-27 15:39:11 -0400

    Update faq-categories index

--- kb/index.php
+++ kb/index.php
@@ -15,8 +15,23 @@
 **********************************************************************/
 require('kb.inc.php');
 require_once(INCLUDE_DIR.'class.category.php');
+$nofind='Not finding what you\'re looking for?';
 $inc='knowledgebase.inc.php';
-require(CLIENTINC_DIR.'header.inc.php');
-require(CLIENTINC_DIR.$inc);
-require(CLIENTINC_DIR.'footer.inc.php');
-?>
+
+require(CLIENTINC_DIR.'header.inc.php'); ?>
+    <div id="features" class="container light">
+    <div class="row page">
+            <?php require(CLIENTINC_DIR.$inc); ?>
+            <div id="faq-search">
+                <form method="get" action="faq.php">
+                    <input type="hidden" name="a" value="search"/>
+                    <label>
+                        <?php echo $nofind; ?><br />
+                        <input type="text" name="q" class="search" placeholder="<?php echo __('Search our knowledge base'); ?>"/>
+                    </label>
+                    <input type="submit" style="display:none" value="search"/>
+                </form>
+            </div>
+        </div>
+    </div>
+<?php require(CLIENTINC_DIR.'footer.inc.php'); ?>
