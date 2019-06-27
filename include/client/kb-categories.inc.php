afcedbb94ae072a51c6d2e639b118d8ebbec7417
Collier Devlin <cdevlin@smartlink.city>
2019-06-27 16:08:15 -0400

    Update layout of kb-categories

--- include/client/kb-categories.inc.php
+++ include/client/kb-categories.inc.php
@@ -8,24 +8,23 @@
         // ->filter(array('faq_count__gt'=>0));
     if ($categories->exists(true)) { ?>
         <div><?php echo __('Click on the category to browse FAQs.'); ?></div>
-        <ul id="kb">
-        <?php foreach ($categories as $C) { ?>
-            <li><i></i>
-            <div style="margin-left:45px">
-            <h4><?php echo sprintf('<a href="faq.php?cid=%d">%s (%d)</a>',
-                $C->getId(), Format::htmlchars($C->getLocalName()), $C->faq_count); ?></h4>
-            <div class="faded" style="margin:10px 0">
-                <?php echo Format::safe_html($C->getLocalDescriptionWithImages()); ?>
-            </div>
-            <?php foreach ($C->faqs
-                    ->exclude(array('ispublished'=>FAQ::VISIBILITY_PRIVATE))
-                    ->limit(5) as $F) { ?>
-                <div class="popular-faq"><i class="icon-file-alt"></i>
-                <a href="faq.php?id=<?php echo $F->getId(); ?>">
-                <?php echo $F->getLocalQuestion() ?: $F->getQuestion(); ?>
-                </a></div>
-            <?php } ?>
-            </div>
+        <ul class="feature-list">
+        <?php foreach ($categories as $C) {
+            if (($p=$C->parent)
+                && ($categories->findFirst(array('category_id' => $p->getId()))))
+            continue;
+
+            $count = $C->faq_count
+        ?>
+            <li>
+                <h4>
+                    <?php
+                        echo sprintf('<a href="faq.php?cid=%d">%s %s</a>',
+                            $C->getId(), Format::htmlchars($C->getLocalName()),
+                            $count ? "({$count})": ''
+                        );
+                    ?>
+                </h4>
             </li>
 <?php   } ?>
        </ul>
@@ -34,4 +33,3 @@
         echo __('No articles found');
     }
 ?>
-</div>
