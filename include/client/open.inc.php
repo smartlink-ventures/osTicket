2b880d28c14bd224849f68229c8804adad689f34
Collier Devlin <cdevlin@smartlink.city>
2019-06-28 09:26:58 -0400

    Update open ticket page

--- include/client/open.inc.php
+++ include/client/open.inc.php
@@ -38,80 +38,89 @@ if ($info['topicId'] && ($topic=Topic::lookup($info['topicId']))) {
   <input type="hidden" name="a" value="open">
   <table width="800" cellpadding="1" cellspacing="0" border="0">
     <tbody>
-<?php
-        if (!$thisclient) {
-            $uform = UserForm::getUserForm()->getForm($_POST);
-            if ($_POST) $uform->isValid();
-            $uform->render(array('staff' => false));
-        }
-        else { ?>
-            <tr><td colspan="2"><hr /></td></tr>
-        <tr><td><?php echo __('Email'); ?>:</td><td><?php
-            echo $thisclient->getEmail(); ?></td></tr>
-        <tr><td><?php echo __('Client'); ?>:</td><td><?php
-            echo Format::htmlchars($thisclient->getName()); ?></td></tr>
-        <?php } ?>
-    </tbody>
+        <?php
+            if (!$thisclient) {
+                $uform = UserForm::getUserForm()->getForm($_POST);
+                if ($_POST) $uform->isValid();
+                $uform->render(array('staff' => false));
+            }
+            else { ?>
+                <tr><td colspan="2"><hr /></td></tr>
+            <tr><td><?php echo __('Email'); ?>:</td><td><?php
+                echo $thisclient->getEmail(); ?></td></tr>
+            <tr><td><?php echo __('Client'); ?>:</td><td><?php
+                echo Format::htmlchars($thisclient->getName()); ?></td></tr>
+            <?php } ?>
+        </tbody>
+    </table>
+    
+<table cellpadding="1" cellspacing="0" border="0">
     <tbody>
-    <tr><td colspan="2"><hr />
-        <div class="form-header" style="margin-bottom:0.5em">
-        <b><?php echo __('Help Topic'); ?></b>
-        </div>
-    </td></tr>
-    <tr>
-        <td colspan="2">
-            <select id="topicId" name="topicId" onchange="javascript:
-                    var data = $(':input[name]', '#dynamic-form').serialize();
-                    $.ajax(
-                      'ajax.php/form/help-topic/' + this.value,
-                      {
-                        data: data,
-                        dataType: 'json',
-                        success: function(json) {
-                          $('#dynamic-form').empty().append(json.html);
-                          $(document.head).append(json.media);
+        <tr><td colspan="2"><hr />
+            <div class="form-header" style="margin-bottom:0.5em">
+            <b><?php echo __('Help Topic'); ?></b>
+            </div>
+        </td></tr>
+        <tr>
+            <td colspan="2">
+                <select id="topicId" name="topicId" onchange="javascript:
+                        var data = $(':input[name]', '#dynamic-form').serialize();
+                        $.ajax(
+                        'ajax.php/form/help-topic/' + this.value,
+                        {
+                            data: data,
+                            dataType: 'json',
+                            success: function(json) {
+                            $('#dynamic-form').empty().append(json.html);
+                            $(document.head).append(json.media);
+                            }
+                        });">
+                    <option value="" selected="selected">&mdash; <?php echo __('Select a Help Topic');?> &mdash;</option>
+                    <?php
+                    if($topics=Topic::getPublicHelpTopics()) {
+                        foreach($topics as $id =>$name) {
+                            echo sprintf('<option value="%d" %s>%s</option>',
+                                    $id, ($info['topicId']==$id)?'selected="selected"':'', $name);
                         }
-                      });">
-                <option value="" selected="selected">&mdash; <?php echo __('Select a Help Topic');?> &mdash;</option>
-                <?php
-                if($topics=Topic::getPublicHelpTopics()) {
-                    foreach($topics as $id =>$name) {
-                        echo sprintf('<option value="%d" %s>%s</option>',
-                                $id, ($info['topicId']==$id)?'selected="selected"':'', $name);
-                    }
-                } else { ?>
-                    <option value="0" ><?php echo __('General Inquiry');?></option>
-                <?php
-                } ?>
-            </select>
-            <font class="error">*&nbsp;<?php echo $errors['topicId']; ?></font>
-        </td>
-    </tr>
-    </tbody>
-    <tbody id="dynamic-form">
-        <?php foreach ($forms as $form) {
-            include(CLIENTINC_DIR . 'templates/dynamic-form.tmpl.php');
+                    } else { ?>
+                        <option value="0" ><?php echo __('General Inquiry');?></option>
+                    <?php
+                    } ?>
+                </select>
+                <font class="error">*&nbsp;<?php echo $errors['topicId']; ?></font>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
+    <table cellpadding="1" cellspacing="0" border="0">
+        <tbody id="dynamic-form">
+            <?php foreach ($forms as $form) {
+                include(CLIENTINC_DIR . 'templates/dynamic-form.tmpl.php');
+            } ?>
+        </tbody>
+    </table>
+
+    <table cellpadding="1" cellspacing="0" border="0">
+        <tbody>
+        <?php
+        if($cfg && $cfg->isCaptchaEnabled() && (!$thisclient || !$thisclient->isValid())) {
+            if($_POST && $errors && !$errors['captcha'])
+                $errors['captcha']=__('Please re-enter the text again');
+            ?>
+        <tr class="captchaRow">
+            <td class="required"><?php echo __('CAPTCHA Text');?>:</td>
+            <td>
+                <span class="captcha"><img src="captcha.php" border="0" align="left"></span>
+                &nbsp;&nbsp;
+                <input id="captcha" type="text" name="captcha" size="6" autocomplete="off">
+                <em><?php echo __('Enter the text shown on the image.');?></em>
+                <font class="error">*&nbsp;<?php echo $errors['captcha']; ?></font>
+            </td>
+        </tr>
+        <?php
         } ?>
-    </tbody>
-    <tbody>
-    <?php
-    if($cfg && $cfg->isCaptchaEnabled() && (!$thisclient || !$thisclient->isValid())) {
-        if($_POST && $errors && !$errors['captcha'])
-            $errors['captcha']=__('Please re-enter the text again');
-        ?>
-    <tr class="captchaRow">
-        <td class="required"><?php echo __('CAPTCHA Text');?>:</td>
-        <td>
-            <span class="captcha"><img src="captcha.php" border="0" align="left"></span>
-            &nbsp;&nbsp;
-            <input id="captcha" type="text" name="captcha" size="6" autocomplete="off">
-            <em><?php echo __('Enter the text shown on the image.');?></em>
-            <font class="error">*&nbsp;<?php echo $errors['captcha']; ?></font>
-        </td>
-    </tr>
-    <?php
-    } ?>
-    <tr><td colspan=2>&nbsp;</td></tr>
+        <tr><td colspan=2>&nbsp;</td></tr>
     </tbody>
   </table>
 <hr/>
